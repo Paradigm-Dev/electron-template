f778bfadc65be4fcef6290d9ccd1aa1aa190e410
Aidan Liddy <42472908+Diddy12310@users.noreply.github.com>
2020-09-30 16:23:59 -0400

    Rebuild

--- src/main.js
+++ src/main.js
@@ -1,48 +1,40 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import '@mdi/font/css/materialdesignicons.css'
-import App from './App.vue'
-import './assets/tailwind.css'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import Store from "./store.js";
+import axios from "axios";
 
-const opts = {
-  icons: {
-    iconfont: 'mdi'
-  },
-  theme: {
-    dark: true,
-    themes: {
-      dark: {
-        primary: '#1976D2',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
-      }
-    }
-  }
-}
+const store = new Store();
 
-Vue.use(Vuetify)
-
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
 Vue.mixin({
   methods: {
     $notify(text) {
-      this.$root.feedback = text
-      this.$root.snackbar = true
-    }
-  }
-})
+      this.$root.notify.text = text;
+      this.$root.notify.is = true;
+      setTimeout(() => {
+        this.$root.notify.text = "";
+        this.$root.notify.is = false;
+      }, 3000);
+    },
+  },
+});
 
 new Vue({
-  render: function(h) { return h(App) },
-  vuetify: new Vuetify(opts),
+  vuetify,
+  render: (h) => h(App),
   data() {
     return {
-      snackbar: false,
-      feedback: ''
-    }
-  }
-}).$mount('div#app')    
+      notify: {
+        text: "",
+        is: false,
+      },
+      user: false,
+      data: false,
+    };
+  },
+  created() {
+    this.$root.types = store.get("objects");
+  },
+}).$mount("#app");
